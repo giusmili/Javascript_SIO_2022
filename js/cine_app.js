50bea222b9e3c197963c8fe693183590d71d2795
giusmili <giusmili67@gmail.com>
2022-03-08 12:36:48 +0100

    image ciné

--- js/cine_app.js
+++ js/cine_app.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
-    let el;
-    el = document.getElementsByTagName("p");
-    for (rows of el) {
-        rows.style.color = "red";
-    }
-    el = document.querySelector("p");
-    el.style.color = "red";
-    el = document.getElementsByTagName("h1")[0];
-    console.log(el);
-    let contents = el.getAttribute("data-title");
-    console.log(contents);
-    el.innerText = contents;
+    let datas = null;
+    console.log(typeof datas);
+    /*   let el
+        el = document.getElementsByTagName("p")       
+        for(rows of el){
+            rows.style.color ="red"
+        } 
+       el = document.querySelector("p")
+       el.style.color ="red"
+        el = document.getElementsByTagName("h1")[0]
+        console.log(el)
+       let contents = el.getAttribute("data-title")
+        console.log(contents)
+        el.innerText = contents */
 
     const Calc = {
         msg: "Le résultat est :",
@@ -22,15 +24,35 @@ document.addEventListener("DOMContentLoaded", e => {
     let r = Calc.print_calc(10);
     console.log(`${Calc.msg} ${r}`);
     console.log(`${Calc.msg.length}`);
-});
 
-/* const User = {
-    el : document.getElementsByTagName("h1")[0],
-    contents : el.getAttribute("data-title"),
-    get_out(){
-        this.el.innerText = this.contents
-        //console.log(this)
-    }
-  }
-User.get_out()
-console.log(User) */
+    const User = {
+        el: document.getElementsByTagName("h1")[0],
+        contents: "",
+        get_out() {
+
+            this.contents = this.el.getAttribute('data-title');
+            //console.log(this)
+        }
+
+    };
+    let prints = User.get_out();
+
+    // structure data movie
+
+    /*  movie:[
+         {
+         image : String,
+         title : String,
+         date: Number,
+         director: String,
+         type : String
+         },
+         {
+         image : String,
+         title : String,
+         date: Number,
+         director: String,
+         type : String
+         },
+     ] */
+});
